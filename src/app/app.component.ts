d4d05b57dcf7845c2076bcba66247ba0a0165c61
Gheorghe Dandiș <dvndis.gheorghe@gmail.com>
2024-07-27 00:08:47 +0300

    code cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
-import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
-import { FuelService } from './services/FuelService/fuel.service';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import {
   FormBuilder,
   FormGroup,
@@ -10,6 +9,8 @@ import {
 } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import * as L from 'leaflet';
+
+import { FuelService } from './services/FuelService/fuel.service';
 import { GeocodingService } from './services/GeoCodingService/geocoding.service';
 import { SortPipe } from './pipes/SortPipe/sort.pipe';
 
@@ -26,7 +27,9 @@ import { SortPipe } from './pipes/SortPipe/sort.pipe';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @ViewChild('mapContainer', { static: false }) mapContainer!: ElementRef;
+
   gasStations: string[] = [
     'Avante',
     'Bemol',
@@ -49,31 +52,37 @@ export class AppComponent {
     'Tirex - (fǎrǎ preț)',
   ];
 
-  title = 'Benzeno';
+  title = 'Benzo';
   searchForm: FormGroup;
   dropdownOpen = false;
   selectedStations: string[] = [];
   errorMessage!: string;
   hasError = false;
   loader = false;
-  fuelResults = [];
+  fuelResults: any[] = [];
+
+  selectedIndex: number | null = null;
+  selectedMarker: L.Marker | null = null;
   map!: L.Map;
-  markers: L.Marker[] = []; // Variabilă pentru markerii curenți
-  latitude = 47.0105; // Coordonatele centrale pentru Moldova
+  markers: L.Marker[] = [];
+  latitude = 47.0105;
   longitude = 28.8638;
   zoom = 7;
 
   currentPage = 1;
-  itemsPerPage = 10;
-  totalItems = 0; // Numărul total de elemente
-  paginatedResults = [];
+  itemsPerPage = 15;
+  totalItems = 0;
+  paginatedResults: any[] = [];
+
+  sortField = '';
+  sortOrder: 'asc' | 'desc' = 'asc';
+
+  currentTheme: string;
 
   constructor(
     private fuelService: FuelService,
     private geocodingService: GeocodingService,
-    private formBuilder: FormBuilder,
-    private router: Router,
-    private route: ActivatedRoute
+    private formBuilder: FormBuilder
   ) {
     this.searchForm = this.formBuilder.group({
       carburant: ['Benzina_Regular', [Validators.required]],
@@ -84,6 +93,7 @@ export class AppComponent {
 
   ngOnInit() {
     this.initMap();
+    this.applySavedTheme();
   }
 
   private initMap(): void {
@@ -108,58 +118,14 @@ export class AppComponent {
         next: (result: any[]) => {
           this.hasError = false;
           this.loader = false;
-          console.log(result);
           this.fuelResults = result;
-          this.totalItems = result.length; // Setează numărul total de elemente
+          this.totalItems = result.length;
 
-          // Curăță markerii anteriori
           this.clearMarkers();
-
-          // Paginați rezultatele
           this.paginateResults();
-
-          // Geocodare pentru fiecare adresă
-          this.paginatedResults.forEach((station) => {
-            const address = `${station.address}, ${station.city}`;
-            this.geocodingService.geocodeAddress(address).subscribe({
-              next: (geoResult: any) => {
-                if (geoResult.results.length > 0) {
-                  const { lat, lng } = geoResult.results[0].geometry;
-
-                  // Creează icon-ul din URL-ul imaginii
-                  const stationIcon = L.icon({
-                    iconUrl: `https://www.peco.md/${station.image}`, // URL-ul imaginii ca icon
-                    iconSize: [32, 32], // Dimensiunea icon-ului (ajustează după necesitate)
-                    iconAnchor: [16, 32], // Punctul de ancorare al icon-ului (ajustează după necesitate)
-                    popupAnchor: [0, -32], // Punctul de ancorare al popup-ului (ajustează după necesitate)
-                  });
-
-                  // Creează HTML-ul pentru popup
-                  const popupContent = `
-                    <div style="width: 100px;">
-                      <img src="https://www.peco.md/${station.image}" alt="${station.gasStation}" style="width: 100%; height: auto;"/>
-                      <p><strong>${station.gasStation}</strong></p>
-                      <p>Preț: ${station.price}</p>
-                      <p>Data: ${station.date}</p>
-                    </div>
-                  `;
-
-                  const marker = L.marker([lat, lng], { icon: stationIcon })
-                    .addTo(this.map)
-                    .bindPopup(popupContent)
-                    .openPopup();
-
-                  this.markers.push(marker);
-                }
-              },
-              error: (error) => {
-                console.error('Geocoding error:', error);
-              },
-            });
-          });
+          this.geocodeStations();
         },
         error: (error) => {
-          console.error(error);
           this.hasError = true;
           this.loader = false;
           this.errorMessage = error.error.error;
@@ -172,16 +138,48 @@ export class AppComponent {
     this.markers = [];
   }
 
+  private geocodeStations() {
+    this.paginatedResults.forEach((station) => {
+      const address = `${station.address}, ${station.city}`;
+      this.geocodingService.geocodeAddress(address).subscribe({
+        next: (geoResult: any) => {
+          if (geoResult.results.length > 0) {
+            const { lat, lng } = geoResult.results[0].geometry;
+            const stationIcon = L.icon({
+              iconUrl: `https://www.peco.md/${station.image}`,
+              iconSize: [32, 32],
+              iconAnchor: [16, 32],
+              popupAnchor: [0, -32],
+            });
+
+            const popupContent = `
+              <div style="width: 100px;">
+                <img src="https://www.peco.md/${station.image}" alt="${station.gasStation}" style="width: 100%; height: auto;"/>
+                <p><strong>${station.gasStation}</strong></p>
+                <p>Preț: ${station.price}</p>
+                <p>Data: ${station.date}</p>
+              </div>
+            `;
+
+            const marker = L.marker([lat, lng], { icon: stationIcon })
+              .addTo(this.map)
+              .bindPopup(popupContent)
+              .openPopup();
+
+            this.markers.push(marker);
+          }
+        },
+        error: (error) => console.error('Geocoding error:', error),
+      });
+    });
+  }
+
   toggleDropdown() {
     this.dropdownOpen = !this.dropdownOpen;
   }
 
   selectAll(event: any) {
-    if (event.target.checked) {
-      this.selectedStations = [...this.gasStations];
-    } else {
-      this.selectedStations = [];
-    }
+    this.selectedStations = event.target.checked ? [...this.gasStations] : [];
   }
 
   selectStation(station: string, event: any) {
@@ -203,13 +201,14 @@ export class AppComponent {
   }
 
   cancel() {
-    this.searchForm.setValue({
+    this.searchForm.reset({
       carburant: 'Benzina_Regular',
       locatie: null,
       nume_locatie: null,
     });
     this.fuelResults = [];
     this.selectedStations = [];
+    this.clearMarkers();
   }
 
   get totalPages(): number {
@@ -218,12 +217,14 @@ export class AppComponent {
 
   private paginateResults() {
     const startIndex = (this.currentPage - 1) * this.itemsPerPage;
-    const endIndex = startIndex + this.itemsPerPage;
-    this.paginatedResults = this.fuelResults.slice(startIndex, endIndex);
+    this.paginatedResults = this.fuelResults.slice(
+      startIndex,
+      startIndex + this.itemsPerPage
+    );
   }
 
   public goToPage(page: number) {
-    if (page > 0 && page <= Math.ceil(this.totalItems / this.itemsPerPage)) {
+    if (page > 0 && page <= this.totalPages) {
       this.currentPage = page;
       this.paginateResults();
     }
@@ -237,28 +238,19 @@ export class AppComponent {
     this.goToPage(this.currentPage - 1);
   }
 
-  selectedIndex: number | null = null;
-  selectedMarker: L.Marker | null = null; // Markerul selectat pe hartă
-
-  // Alte metode existente
-
   highlightStation(station: any) {
-    // Setează indexul rândului selectat
     this.selectedIndex = this.paginatedResults.indexOf(station);
 
-    // Geocodare pentru adresa stației selectate
     const address = `${station.address}, ${station.city}`;
     this.geocodingService.geocodeAddress(address).subscribe({
       next: (geoResult: any) => {
         if (geoResult.results.length > 0) {
           const { lat, lng } = geoResult.results[0].geometry;
 
-          // Dacă există un marker anterior selectat, elimină-l de pe hartă
           if (this.selectedMarker) {
             this.map.removeLayer(this.selectedMarker);
           }
 
-          // Creează un nou marker pentru stația selectată
           const stationIcon = L.icon({
             iconUrl: `https://www.peco.md/${station.image}`,
             iconSize: [32, 32],
@@ -276,40 +268,50 @@ export class AppComponent {
                 <p>Preț: ${station.price}</p>
                 <p>Data: ${station.date}</p>
               </div>
-            `
+              `
             )
             .openPopup();
 
-          // Mutați centrul hărții pe locația stației selectate
-          this.map.setView([lat, lng], 13); // Ajustează nivelul de zoom după necesitate
+          this.map.setView([lat, lng], 13);
+
+          this.scrollToMap();
         }
       },
-      error: (error) => {
-        console.error('Geocoding error:', error);
-      },
+      error: (error) => console.error('Geocoding error:', error),
     });
   }
 
-  sortField: string = '';
-  sortOrder: 'asc' | 'desc' = 'asc';
+  private scrollToMap() {
+    this.mapContainer.nativeElement.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  }
 
   sort(field: string) {
-    // Toggle sort order if sorting by the same field
-    if (this.sortField === field) {
-      this.sortOrder = this.sortOrder === 'asc' ? 'desc' : 'asc';
-    } else {
-      this.sortField = field;
-      this.sortOrder = 'asc';
-    }
-
-    // Sort the results
+    this.sortField = field;
+    this.sortOrder = this.sortOrder === 'asc' ? 'desc' : 'asc';
     this.fuelResults = new SortPipe().transform(
       this.fuelResults,
       this.sortField,
       this.sortOrder
     );
-
-    // Reapply pagination
     this.paginateResults();
+  }
+
+  applySavedTheme(): void {
+    const savedTheme = localStorage.getItem('theme');
+    this.currentTheme = savedTheme || 'light';
+    document
+      .getElementsByTagName('html')[0]
+      .setAttribute('data-theme', this.currentTheme);
+  }
+
+  toggleTheme() {
+    this.currentTheme = this.currentTheme === 'light' ? 'dark' : 'light';
+    document
+      .getElementsByTagName('html')[0]
+      .setAttribute('data-theme', this.currentTheme);
+    localStorage.setItem('theme', this.currentTheme);
   }
 }
