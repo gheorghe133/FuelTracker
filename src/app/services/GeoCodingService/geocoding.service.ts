c595c959e89c0e646c4917fa66285c18235bee44
Gheorghe Dandiș <dvndis.gheorghe@gmail.com>
2024-08-31 00:41:13 +0300

    hide sensitive data

--- src/app/services/GeoCodingService/geocoding.service.ts
+++ src/app/services/GeoCodingService/geocoding.service.ts
@@ -2,23 +2,23 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+import { environment } from '../../../environments/environment.development';
+
 @Injectable({
   providedIn: 'root',
 })
 export class GeocodingService {
-  private apiKey = '890b833542b747058c0073b1611c4160';
-  private apiUrl = 'https://api.opencagedata.com/geocode/v1/json';
+  private readonly apiUrl = `${environment.server}/geocode`;
 
   constructor(private http: HttpClient) {}
 
   geocodeAddress(address: string): Observable<any> {
-    const url = `${this.apiUrl}?q=${encodeURIComponent(address)}&key=${this.apiKey}&pretty=1`;
-    
+    const url = `${this.apiUrl}?address=${encodeURIComponent(address)}`;
     return this.http.get(url);
   }
 
   reverseGeocode(lat: number, lng: number): Observable<any> {
-    const url = `${this.apiUrl}?q=${lat}+${lng}&key=${this.apiKey}&language=ro&pretty=1`;
+    const url = `${this.apiUrl}?lat=${lat}&lng=${lng}`;
     return this.http.get(url);
   }
 }
